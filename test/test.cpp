9361844762dfb82de8f7f47e2a1024ff25f419f0
Michael-Steven <727950731@qq.com>
2020-03-10 18:12:22 +0800

    style&&fdocs: Adjust
    Adjust directory structure and add readme.md.

--- test/test.cpp
+++ test/test.cpp
@@ -7,25 +7,25 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 namespace test {
 	TEST_CLASS(testlines) {
 public:
-	TEST_METHOD(TestLines1) {
-		Intersect* field = new Intersect();
-		field->add_line(0, 0, 1, 1);
-		Assert::AreEqual(0, field->get_result());
-	}
-	TEST_METHOD(TestLines2) {
-		Intersect* field = new Intersect();
-		field->add_line(0, 0, 1, 1);
-		field->add_line(1, 0, 0, 1);
-		field->add_line(1, 2, 2, 0);
-		Assert::AreEqual(3, field->get_result());
-	}
-	TEST_METHOD(TestLines3) {
-		Intersect* field = new Intersect();
-		field->add_line(0, 0, 1, 1);
-		field->add_line(0, 0, 0, 1);
-		field->add_line(0, 0, -1, 0);
-		Assert::AreEqual(1, field->get_result());
-	}
+		TEST_METHOD(TestLines1) {
+			Intersect* field = new Intersect();
+			field->add_line(0, 0, 1, 1);
+			Assert::AreEqual(0, field->get_result());
+		}
+		TEST_METHOD(TestLines2) {
+			Intersect* field = new Intersect();
+			field->add_line(0, 0, 1, 1);
+			field->add_line(1, 0, 0, 1);
+			field->add_line(1, 2, 2, 0);
+			Assert::AreEqual(3, field->get_result());
+		}
+		TEST_METHOD(TestLines3) {
+			Intersect* field = new Intersect();
+			field->add_line(0, 0, 1, 1);
+			field->add_line(0, 0, 0, 1);
+			field->add_line(0, 0, -1, 0);
+			Assert::AreEqual(1, field->get_result());
+		}
 	};
 	TEST_CLASS(testcircles) {
 		TEST_METHOD(TestCircles1) {
